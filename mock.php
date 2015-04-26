f3f2ecccae0683edb7b242f529d214fcb2e3f3fb
Sumeet Lalla <sumeetlalla1994_@bitbucket.org>
2015-04-27 04:25:06 +0530

    Initial commit

---
+++ mock.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dlf164
+ * Date: 26-04-2015
+ * Time: 19:31
+ */
+$r=$_POST['u'];
+if($r=="Mock1")
+{
+
+}
+if($r=="Mock2")
+{
+
+}
+if($r=="Mock3")
+{
+
+}
+if($r=="Mock4")
+{
+
+}
+if($r=="Mock5")
+{
+
+}
